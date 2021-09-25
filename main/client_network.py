c3129e1b1d3a883daafb4c308d879ce817d26012
xtradark812 <hadyenayef@icloud.com>
2021-09-25 18:48:54 +0900

    server sends data
    server can now send data to each client of other clients pos, but client cannot draw yet

--- main/client_network.py
+++ main/client_network.py
@@ -1,5 +1,6 @@
 import socket
 import json
+import threading
 
 class Network():
     def __init__(self):
@@ -10,6 +11,11 @@ class Network():
         self.BUFSIZ = 2048
         self.client.connect(self.addr)
         self.username = self.connectionInit("test")
+        self.enemyUsername = None
+        self.enemyPos = None
+        self.waitForEnemyThread = threading.Thread(target=self.waitForBattle)
+        self.waitForEnemyThread.start()
+
 
     #def getPos(self):
      #   return self.pos
@@ -39,4 +45,50 @@ class Network():
             self.client.sendall(bytes(self.serialized, "utf8")) ### SENDS DATA
         except socket.error as e:
             print ("Send error:", e)
+    
+    def waitForBattle(self):
+        while True:
+            try:
+                response = json.loads(self.client.recv(self.BUFSIZ).decode("utf8")) ### WAITS FOR DATA TO BE RETURNED
+                if response["requestType"]=="battleReq": 
+                    print("starting a battle with", response["enemyU"])
+                    self.accept = {"requestType":"battleReq","battleAccepted":True}
+                    self.serialized = json.dumps(self.accept) #serialize data
+                    self.client.sendall(bytes(self.serialized, "utf8")) 
+                    self.enemyUsername = response["enemyU"]
+                    getEnemyPosThread = threading.Thread(target=self.recivePosData)
+                    break
+            except:
+                print("Invalid response from server")
+
+    def checkEnemyConnected(self):
+        if self.enemyUsername != None:
+            return self.enemyUsername
+        else:
+            return False
+
+    def recivePosData(self):
+        self.data = {}
+        while True:
+            try: 
+                self.recv = self.client.recv(self.BUFSIZ).decode("utf8")
+                self.data = json.loads(self.recv)
+                if self.data["requestType"] == "enemyLoc":
+                    self.enemyPos = self.data
+
+                #check if data being recived is pos data?
+            except Exception as e:
+                print("error:",e)
+            
+            # if not self.recv:
+            #     print("Disconnected")
+            #     print(self.addr, "has lost connection")
+            #     self.client.close()
+            #     break
+    def getEnemyPos(self):
+        return self.enemyPos
+            
+
+        #this waits for an enemy to connect, and once it connects it will constantly receive pos
+    
 
